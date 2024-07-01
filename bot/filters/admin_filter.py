620352d26e843053e07ef320791979129106bd54
Kirill Mikhailov <kicode@mail.ru>
2024-07-01 11:23:47 +0300

    register_filter

--- bot/filters/admin_filter.py
+++ bot/filters/admin_filter.py
@@ -10,17 +10,20 @@ class IsAdmin(Filter):
     """
 
     async def __call__(self, update: Message | CallbackQuery, bot: Bot) -> bool:
+        try:
+            group_id = None
+            if isinstance(update, CallbackQuery):
+                group_id = update.message.chat.id
+            elif isinstance(update, Message):
 
-        group_id = None
-        if isinstance(update, CallbackQuery):
-            group_id = update.message.chat.id
-        elif isinstance(update, Message):
+                group_id = update.chat.id
+            if group_id:
+                user_status = (await bot.get_chat_member(
+                    chat_id=group_id,
+                    user_id=update.from_user.id
+                )).status
 
-            group_id = update.chat.id
-        if group_id:
-            user_status = (await bot.get_chat_member(
-                chat_id=group_id,
-                user_id=update.from_user.id
-            )).status
+                return user_status in {ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR}
+        except Exception as e:
 
-            return user_status in {ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR}
+            print(e)
